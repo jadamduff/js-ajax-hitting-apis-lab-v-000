8b5168205280f384ad40d4c4dc1d3883b93730a1
Adam Duff <j.adam.duff@gmail.com>
2018-11-04 11:57:49 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', 'https://api.github.com/users/' + document.getElementById('username').value + '/repos');
+  req.send();
+}
+
+function displayRepositories() {
+  let repos = JSON.parse(this.responseText);
+  console.log(repos);
+  let repoList = '<ul>';
+  for (var i = 0; i < repos.length; i++) {
+    repoList += '<li><a href="' + repos[i]['html_url'] + '">' + repos[i]['name'] + '</a> <a href="#" data-url="' + repos[i]['url'] + '" onclick="getCommits(this)">Commits</a></li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el) {
+  console.log('worked');
+  let url = el.dataset.url;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', url + '/commits');
+  req.send();
+}
+
+function displayCommits() {
+  let commits = JSON.parse(this.responseText);
+  console.log(commits);
+}
